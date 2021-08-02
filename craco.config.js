05f276beac90b2d9a6d869ce7c961a0e2cfa0d53
rahalrazika <rahalraika@gmail.com>
2021-08-02 05:01:24 +0200

    style the home page

--- craco.config.js
+++ craco.config.js
@@ -1,9 +1,10 @@
-/* eslint-disable global-require */
-// craco.config.js
 module.exports = {
   style: {
     postcss: {
-      plugins: [require('tailwindcss'), require('autoprefixer')],
+      plugins: [
+        require('tailwindcss'),
+        require('autoprefixer'),
+      ],
     },
   },
 };
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Header from '../components/Header';
+import logo from '../assets/logo.png';
 
 export default function HomePage() {
   return (
     <div className="homePage px-16">
       <Header />
       <div className=" text-4xl uppercase mt-24 ">
-        <span className="main-text">rem Ipsum is simply dummy text of the printing.</span>
+        <img src={logo} alt="banner" className="main-img" />
+        <span className="main-text">Find your Successful Partnership</span>
       </div>
     </div>
   );
