15f1cbeed93c7563841d998261ecddde199474c7
rahalrazika <rahalraika@gmail.com>
2021-08-03 12:13:25 +0200

    fix linters errors

--- src/layouts/navLayout.js
+++ src/layouts/navLayout.js
@@ -1,3 +1,4 @@
+/* eslint-disable react/destructuring-assignment */
 import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import NavBar from '../components/NavBar';
