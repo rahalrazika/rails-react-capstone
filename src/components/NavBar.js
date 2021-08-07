d24137ef82ed1d54872024e7ed4946aa25552cae
rahalrazika <rahalraika@gmail.com>
2021-08-07 22:27:41 +0200

    style the projects detail page

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -9,11 +9,11 @@ function NavBar() {
       <div className="bg-gray-100 text-white w-64 px-2 ">
         <div>
           <img
-            className="inline-block object-cover w-24 h-24 rounded-full  mt-6 mb-4 "
+            className="inline-block object-cover mr-2 bg-gray-100 w-24 h-24 rounded-full  mt-6 mb-4 "
             src={user}
             alt="user"
           />
-          <div className="text-gray-500 font-semibold uppercase space-y-6 ">
+          <div className="text-gray-500 font-semibold uppercase space-y-6  ">
             Wesley Larson
           </div>
         </div>
--- src/pages/ProjectDetail.js
+++ src/pages/ProjectDetail.js
@@ -1,31 +1,58 @@
 import React from 'react';
-import { BsHeart, BsHeartFill } from 'react-icons/all';
-import port from '../assets/por1.png';
+import { BsHeart, BsHeartFill, AiOutlineArrowLeft } from 'react-icons/all';
+// import port from '../assets/por.png';
+import port2 from '../assets/proj.jpg';
+// import port2 from '../assets/por3.jpg';
+import user from '../assets/user.png';
 
 function ProjectDetail() {
   return (
-    <div className="relative h-screen flex">
-      <div className="flex-1 p-10">
-        <div className="container flex justify-center">
-          <div className="max-w-sm py-32">
-            <div className="bg-white relative shadow-lg hover:shadow-xl transition duration-500 rounded-lg">
-              <img className="rounded-t-lg" src={port} alt="" />
-              <div className="py-6 px-8 rounded-lg bg-white">
-                <h1 className="text-gray-700 font-bold text-2xl mb-3 capitalize rounded transition duration-100  hover:bg-yellow-400 hover:text-white hover:cursor-pointer ">
-                  project name
-                </h1>
-
-                <div
-                  className="mt-6 py-2 px-4 text-gray-800 font-bold "
-                >
-                  <span className="text-md">$150</span>
-                </div>
-              </div>
-              <div className=" top-2 right-2 py-2 px-4 bg-white rounded-lg">
-                <BsHeart />
-                <BsHeartFill className="text-yellow-300" />
-              </div>
-            </div>
+    <div className="w-full h-screen">
+      <div className="bg-gray-100 h-7  flex ">
+        <AiOutlineArrowLeft className="text-2xl" />
+      </div>
+      <div className="grid grid-cols-1 sm:grid-cols-2 sm:h-screen sm:px-8 sm:py-12 sm:gap-x-8 md:py-16">
+        <div className="relative text-yellow-700 flex justify-between z-10 col-start-1 row-start-1 px-4 pt-40 pb-3 bg-gradient-to-t from-black sm:bg-none">
+          <h2 className="text-xl font-semibold text-white sm:text-2xl sm:leading-7 sm:text-black md:text-3xl">
+            Project Name
+          </h2>
+        </div>
+        <div className="col-start-1 row-start-2 px-4 sm:pb-16">
+          <div className="flex items-center text-sm font-medium my-5 sm:mt-2 sm:mb-4">
+            <BsHeart />
+            <BsHeartFill className="text-yellow-300" />
+          </div>
+        </div>
+        <div className="col-start-1 row-start-3 space-y-3 px-4">
+          <h3 className="text-center font-semibold text-xl">Price$</h3>
+          <p className="flex items-center text-black text-sm font-medium">
+            <img
+              src={user}
+              alt="user"
+              className="w-6 h-6 rounded-full mr-2 bg-gray-100"
+            />
+            username
+          </p>
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ea,
+            pariatur officia minus perferendis dolores sunt doloremque
+            laudantium hic eos, aliquid magni soluta possimus recusandae tempore
+            officiis corrupti voluptatum sit excepturi!
+          </p>
+          <button
+            type="button"
+            className="bg-yellow-100  text-yellow-700 text-base font-semibold px-6 mt-11 py-2 rounded-lg"
+          >
+            Apply For partnership
+          </button>
+        </div>
+        <div className="col-start-1 row-start-1 flex sm:col-start-2 sm:row-span-3">
+          <div className="w-full relative grid grid-cols-1  gap-2">
+            <img
+              src={port2}
+              alt=""
+              className="absolute inset-0 w-full h-full object-cover bg-gray-100 sm:rounded-lg "
+            />
           </div>
         </div>
       </div>
