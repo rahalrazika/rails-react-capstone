ef301e94ba771f08cb437d91339b4263ec4c7fa9
rahalrazika <rahalraika@gmail.com>
2021-08-16 13:57:56 +0200

    Style the empty page

--- src/pages/Favourite.js
+++ src/pages/Favourite.js
@@ -29,24 +29,44 @@ const Fav = ({
     fetchProjects(window.localStorage.getItem('token'));
     getFavourites(window.localStorage.getItem('token'));
   }, []);
+  let renderList;
+  if (projectsToShow.length !== 0) {
+    renderList = (
+      <>
+        <h1 className="text-yellow-400 m-0 mb-16 text-2xl font-semibold uppercase">
+          Favourites List
+        </h1>
+        {projectsToShow.map((el) => (
+          <div key={el.id} className=" min-w-full px-20 mb-9">
+            <div className="bg-white p-8 rounded-lg shadow-lg  hover:shadow-2xl transition duration-500">
+              <h1 className="text-2xl text-gray-800 font-semibold mb-3">
+                {el.name}
+              </h1>
+              <button
+                onClick={() => removeFavourite(el, favourites, removeFavouriteFromRedux)}
+                type="button"
+                className="py-2 px-4 mt-8 bg-red-500 text-white rounded-md shadow-xl"
+              >
+                Remove
+              </button>
+            </div>
+          </div>
+        ))}
+      </>
+    );
+  } else {
+    renderList = (
+
+      <div className="bg-blue-100 border-t border-b w-full h-full border-blue-500 text-blue-700 px-4 py-3">
+        <p className="font-bold text-6xl my-16">Oups!!</p>
+        <p className="text-5xl">The list is Empty!</p>
+      </div>
+    );
+  }
   return (
     <div className="flex-1 min-h-screen flex flex-col items-center justify-center">
-      {projectsToShow.map((el) => (
-        <div key={el.id} className=" min-w-full px-20 mb-9">
-          <div className="bg-white p-8 rounded-lg shadow-lg  hover:shadow-2xl transition duration-500">
-            <h1 className="text-2xl text-gray-800 font-semibold mb-3">
-              {el.name}
-            </h1>
-            <button
-              onClick={() => removeFavourite(el, favourites, removeFavouriteFromRedux)}
-              type="button"
-              className="py-2 px-4 mt-8 bg-red-500 text-white rounded-md shadow-xl"
-            >
-              Remove
-            </button>
-          </div>
-        </div>
-      ))}
+
+      {renderList}
     </div>
   );
 };
--- src/pages/Partnership.js
+++ src/pages/Partnership.js
@@ -33,7 +33,6 @@ const Partnership = ({
   const partnershipId = partnerships.find(
     (item) => `${item.project_id}`,
   );
-
   async function RemovePartnership() {
     const token = window.localStorage.getItem('token');
     const config = {
@@ -51,30 +50,52 @@ const Partnership = ({
       throw new Error(error);
     }
   }
+
+  let renderList;
+  if (projectsList.length !== 0) {
+    renderList = (
+      <>
+        <h1 className="text-yellow-400 m-0 mb-16 text-2xl font-semibold uppercase">
+          Partnerships List
+        </h1>
+
+        {projectsList.map((el) => (
+          <div key={el.id} className=" min-w-full px-20 mb-9">
+            <div className="bg-white p-8 rounded-lg shadow-lg  hover:shadow-2xl transition duration-500">
+              <h1 className="text-2xl text-gray-800 font-semibold mb-3">
+                {el.name}
+              </h1>
+              <button
+                onClick={() => RemovePartnership(partnershipId.id)}
+                type="button"
+                className="py-2 mr-2 px-4 mt-8 bg-red-500 text-white rounded-md shadow-xl"
+              >
+                Remove
+              </button>
+              <button
+                type="button"
+                className="py-2 px-4 mt-8 bg-blue-500 text-white rounded-md shadow-xl"
+              >
+                Get link To the zoom call
+              </button>
+            </div>
+          </div>
+        ))}
+      </>
+    );
+  } else {
+    renderList = (
+
+      <div className="bg-blue-100 border-t border-b w-full h-full border-blue-500 text-blue-700 px-4 py-3">
+        <p className="font-bold text-6xl my-16">Oups!!</p>
+        <p className="text-5xl">The list is Empty!</p>
+      </div>
+    );
+  }
   return (
     <div className="flex-1 min-h-screen flex flex-col items-center justify-center">
-      {projectsList.map((el) => (
-        <div key={el.id} className=" min-w-full px-20 mb-9">
-          <div className="bg-white p-8 rounded-lg shadow-lg  hover:shadow-2xl transition duration-500">
-            <h1 className="text-2xl text-gray-800 font-semibold mb-3">
-              {el.name}
-            </h1>
-            <button
-              onClick={() => RemovePartnership(partnershipId.id)}
-              type="button"
-              className="py-2 mr-2 px-4 mt-8 bg-red-500 text-white rounded-md shadow-xl"
-            >
-              Remove
-            </button>
-            <button
-              type="button"
-              className="py-2 px-4 mt-8 bg-blue-500 text-white rounded-md shadow-xl"
-            >
-              Get link To the zoom call
-            </button>
-          </div>
-        </div>
-      ))}
+
+      {renderList}
     </div>
   );
 };
